14d8692f5b7c16809ad1a4f5d92a823a7d0fb5cf
syshim77 <syshim77@gmail.com>
2017-12-03 15:00:58 +0900

    done chapter 18

--- iOS_programming/TouchTracker/DrawView.swift
+++ iOS_programming/TouchTracker/DrawView.swift
@@ -8,9 +8,18 @@
 
 import UIKit
 
-class DrawView: UIView {
+class DrawView: UIView, UIGestureRecognizerDelegate {
     var currentLines = [NSValue: Line]()
     var finishedLines = [Line]()
+    var selectedLineIndex: Int? {
+        didSet {
+            if selectedLineIndex == nil {
+                let menu = UIMenuController.shared
+                menu.setMenuVisible(false, animated: true)
+            }
+        }
+    }
+    var moveRecognizer: UIPanGestureRecognizer!
     
     @IBInspectable var finishedLineColor: UIColor = UIColor.black {
         didSet {
@@ -28,6 +37,82 @@ class DrawView: UIView {
         }
     }
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        let doubleTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(doubleTap(gestureRecognizer: )))
+        doubleTapRecognizer.numberOfTapsRequired = 2
+        doubleTapRecognizer.delaysTouchesBegan = true
+        addGestureRecognizer(doubleTapRecognizer)
+        
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(tap(gestureRecognizer: )))
+        tapRecognizer.delaysTouchesBegan = true
+        tapRecognizer.require(toFail: doubleTapRecognizer)
+        addGestureRecognizer(tapRecognizer)
+        
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(longPress(gestureRecognizer: )))
+        addGestureRecognizer(longPressRecognizer)
+        
+        moveRecognizer = UIPanGestureRecognizer(target: self, action: #selector(moveLine(gestureRecognizer:)))
+        moveRecognizer.delegate = self
+        moveRecognizer.cancelsTouchesInView = false
+        addGestureRecognizer(moveRecognizer)
+    }
+    
+    @objc func tap(gestureRecognizer: UIGestureRecognizer) {
+        print("Recognized a tap")
+        
+        let point = gestureRecognizer.location(in: self)
+        selectedLineIndex = indexOfLineAtPoint(point: point)
+        
+        // 메뉴 컨트롤러를 얻는다
+        let menu = UIMenuController.shared
+        
+        if selectedLineIndex != nil {
+            // DrawView를 메뉴 아이템 액션 메시지의 타깃으로 만든다
+            becomeFirstResponder()
+            
+            // "Delete" UIMenuItem을 만든다
+            let deleteItem = UIMenuItem(title: "Delete", action: #selector(deleteLine(sender:)))
+            menu.menuItems = [deleteItem]
+            
+            // 메뉴가 나타날 영역을 정하고 그 위치에 보이게끔 설정한다
+            menu.setTargetRect(CGRect(x: point.x, y: point.y, width: 2, height: 2), in: self)
+            menu.setMenuVisible(true, animated: true)
+        } else {
+            // 선택된 선이 없다면 메뉴를 숨긴다
+            menu.setMenuVisible(false, animated: true)
+        }
+        
+        setNeedsDisplay()
+    }
+    
+    @objc func doubleTap(gestureRecognizer: UIGestureRecognizer) {
+        print("Recognized a double tap")
+        
+        selectedLineIndex = nil
+        currentLines.removeAll(keepingCapacity: false)
+        finishedLines.removeAll(keepingCapacity: false)
+        setNeedsDisplay()
+    }
+    
+    @objc func longPress(gestureRecognizer: UIGestureRecognizer) {
+        print("Recognized a long press")
+        
+        if gestureRecognizer.state == .began {
+            let point = gestureRecognizer.location(in: self)
+            selectedLineIndex = indexOfLineAtPoint(point: point)
+            
+            if selectedLineIndex != nil {
+                currentLines.removeAll(keepingCapacity: false)
+            }
+        } else if gestureRecognizer.state == .ended {
+            selectedLineIndex = nil
+        }
+        
+        setNeedsDisplay()
+    }
+    
     func strokeLine(line: Line) {
         let path = UIBezierPath()
         path.lineWidth = LineThickness
@@ -36,6 +121,73 @@ class DrawView: UIView {
         path.move(to: line.begin)
         path.addLine(to: line.end)
         path.stroke()
+    }
+    
+    func indexOfLineAtPoint(point: CGPoint) -> Int? {
+        // point와 가까운 선을 찾는다
+        for (index, line) in finishedLines.enumerated() {
+            let begin = line.begin
+            let end = line.end
+            
+            // 선 위의 포인트를 검사한다
+            for t in stride(from: CGFloat(0), to: 1.0, by: 0.05) {
+                let x = begin.x + ((end.x - begin.x) * t)
+                let y = begin.y + ((end.y - begin.y) * t)
+                
+                // 탭한 지점이 선과 20포인트 이내라면 이 선을 반환한다
+                if hypot(x - point.x, y - point.y) < 20.0 {
+                    return index
+                }
+            }
+        }
+        
+        // 탭한 지점과 가까운 선이 없다면 선을 선택하지 않는다
+        return nil
+    }
+    
+    @objc func deleteLine(sender: AnyObject) {
+        // finishedLines 목록에서 선택한 선을 제거한다
+        if let index = selectedLineIndex {
+            finishedLines.remove(at: index)
+            selectedLineIndex = nil
+            
+            // 다시 그린다
+            setNeedsDisplay()
+        }
+    }
+    
+    @objc func moveLine(gestureRecognizer: UIPanGestureRecognizer) {
+        print("Recognized a pan")
+        
+        // 선이 선택되면...
+        if let index = selectedLineIndex {
+            // 팬 인식기의 위치가 변하면
+            if gestureRecognizer.state == .changed {
+                // 팬이 얼마나 움직였나?
+                let translation = gestureRecognizer.translation(in: self)
+                
+                // 선의 현재 시작과 끝 지점에 이동 값을 더한다
+                // 오타 없이 복사/붙여 넣기를 했는지 확인한다!
+                finishedLines[index].begin.x += translation.x
+                finishedLines[index].begin.y += translation.y
+                finishedLines[index].end.x += translation.x
+                finishedLines[index].end.y += translation.y
+                
+                gestureRecognizer.setTranslation(CGPoint.zero, in: self)
+                
+                // 화면을 다시 그린다
+                setNeedsDisplay()
+            }
+        } else {
+            // 선택된 선이 없으면 아무것도 하지 않는다
+            return
+        }
+    }
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer,
+       shouldRecognizeSimultaneouslyWithGestureRecognizer
+        otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
     
     override func draw(_ rect: CGRect) {
@@ -48,11 +200,17 @@ class DrawView: UIView {
         for (_, line) in currentLines {
             strokeLine(line: line)
         }
+        
+        if let index = selectedLineIndex {
+            UIColor.green.setStroke()
+            let selectedLine = finishedLines[index]
+            strokeLine(line: selectedLine)
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         // 이벤트의 순서를 확인하기 위해 로그문을 넣어보자
-        print(_FUNCTION_)
+        print(#function)
         
         for touch in touches {
             let location = touch.location(in: self)
@@ -68,7 +226,7 @@ class DrawView: UIView {
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         // 이벤트의 순서를 확인하기 위해 로그문을 넣어보자
-        print(_FUNCTION_)
+        print(#function)
         
         for touch in touches {
             let key = NSValue(nonretainedObject: touch)
@@ -80,7 +238,7 @@ class DrawView: UIView {
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         // 이벤트의 순서를 확인하기 위해 로그문을 넣어보자
-        print(_FUNCTION_)
+        print(#function)
         
         for touch in touches {
             let key = NSValue(nonretainedObject: touch)
@@ -97,10 +255,14 @@ class DrawView: UIView {
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         // 이벤트의 순서를 확인하기 위해 로그문을 넣어보자
-        print(_FUNCTION_)
+        print(#function)
         
         currentLines.removeAll()
         
         setNeedsDisplay()
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
     }
 }
